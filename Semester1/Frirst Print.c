d5fd985091ebdbc484edbe08ea86fa80977ebe80
Aaban-Saad <aabansaad@gmail.com>
2022-09-18 10:08:43 +0600

    First Commit

---
+++ Semester1/Frirst Print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n\tWelcome to NSU!");
+    getchar();
+    return 0;
+}
---
+++ Semester1/Input Output.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    float frac;
+    char name[20];
+
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    printf("The number is %d", num);
+
+    printf("\n\nEnter a fractional number: ");
+    scanf("%f",&frac);
+    printf("The number is %f", frac);
+
+    printf("\n\nEnter your name: ");
+    scanf("%s",&name);
+    printf("Your name is %s", name);
+    getchar();
+    getchar();
+    return 0;
+}

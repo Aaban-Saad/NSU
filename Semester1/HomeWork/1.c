b96b1c13bafecbad22e7ede304b64f8edc29740c
Aaban-Saad <aabansaad@gmail.com>
2022-10-07 10:21:11 +0600

    Home Works

---
+++ Semester1/HomeWork/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n\tSL\tProduct\t\tQuantity\tPrice");
+    printf("\n\t1\tOil\t\t1 Liter\t\t200 Tk");
+    printf("\n\t2\tSugar\t\t1 Kg\t\t100 Tk");
+    printf("\n\t3\tRice\t\t5 Kg\t\t550 Tk");
+    printf("\n\t4\tAlmonds\t\t35 g\t\t130 Tk\n\n");
+    return 0;
+}
---
+++ Semester1/HomeWork/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("\n\tEnter a character: ");
+    scanf("%c", &a);
+    printf("\tASCII value of '%c' is %d\n\n", a, a);
+    return 0;
+}
